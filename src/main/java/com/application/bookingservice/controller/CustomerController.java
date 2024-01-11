376480612dc500d64b3ebdcad3738e6df09b4715
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-11 15:20:33 +0200

    added swagger to customer controller

--- src/main/java/com/application/bookingservice/controller/CustomerController.java
+++ src/main/java/com/application/bookingservice/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.application.bookingservice.controller;
 
 import com.application.bookingservice.service.customer.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/customers")
+@Tag(name = "Customer management.", description = "Endpoints for managing customers.")
 public class CustomerController {
     private CustomerService customerService;
 
     @PutMapping("/{id}/role")
+    @Operation(summary = "Update roles.", description = "Enables customers to update their roles, providing role-based access.")
     public Object updateRole(@PathVariable Long id,
                              @RequestBody @Valid Object updateCustomerRoleRequestDto) {
         return customerService.updateRole(id, updateCustomerRoleRequestDto);
     }
 
     @GetMapping("/me")
+    @Operation(summary = "Get customer.", description = "Retrieves the profile information for the currently logged-in customer.")
     public Object getCustomer() {
         //Customer customer = (Customer) authentication.getPrincipal();
         Long customerId = 1L;
@@ -30,6 +35,7 @@ public class CustomerController {
     }
 
     @PutMapping("/me")
+    @Operation(summary = "Update profile information.", description = "Allows customers to update their profile information.")
     public Object updateProfileInfo(@RequestBody @Valid Object customerRequestDto) {
         //Customer customer = (Customer) authentication.getPrincipal();
         Long customerId = 1L;
