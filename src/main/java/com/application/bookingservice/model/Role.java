0f7471018f157908b5752710e5ea873f63a09635
Bohdan Bilko <rapsbodik@gmail.com>
2024-01-11 16:02:57 +0200

    added Customer and Role entities

---
+++ src/main/java/com/application/bookingservice/model/Role.java
@@ -0,0 +1,34 @@
+package com.application.bookingservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private RoleName role;
+
+    @Override
+    public String getAuthority() {
+        return role.name();
+    }
+
+    public enum RoleName {
+        ROLE_MANAGER,
+        ROLE_CUSTOMER
+    }
+}
