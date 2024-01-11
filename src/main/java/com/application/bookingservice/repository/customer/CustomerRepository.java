7e3fd5366babd0d827e5f55dd385a8f1c38f50a1
Bohdan Bilko <rapsbodik@gmail.com>
2024-01-11 16:09:19 +0200

    added CustomerRepository class and findByEmail() method in it

---
+++ src/main/java/com/application/bookingservice/repository/customer/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.application.bookingservice.repository.customer;
+
+import com.application.bookingservice.model.Customer;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+}
