1a25ae4455db8cb40c6ff23488c30b29a8f6099f
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-11 15:31:40 +0200

    added swagger to booking controller

--- src/main/java/com/application/bookingservice/controller/BookingController.java
+++ src/main/java/com/application/bookingservice/controller/BookingController.java
@@ -1,6 +1,8 @@
 package com.application.bookingservice.controller;
 
 import com.application.bookingservice.service.booking.BookingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/bookings")
+@Tag(name = "Booking management.", description = "Endpoints for managing bookings.")
 public class BookingController {
     private BookingService bookingService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create new booking.", description = "Permits the creation of new accommodation bookings.")
     public Object create(@RequestBody @Valid Object requestBookingDto) {
         //Customer customer = (Customer) authentication.getPrincipal();
         Long customerId = 1L;
@@ -32,6 +36,7 @@ public class BookingController {
     }
 
     @GetMapping("/my")
+    @Operation(summary = "Get all customer bookings", description = "Retrieves customer bookings.")
     public List<Object> getAll(Pageable pageable) {
         //Customer customer = (Customer) authentication.getPrincipal();
         Long customerId = 1L;
@@ -39,22 +44,26 @@ public class BookingController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get booking by id.", description = "Provides information about a specific booking.")
     public Object findById(@PathVariable Long id) {
         return bookingService.findById(id);
     }
 
     @PutMapping("{id}")
+    @Operation(summary = "Update booking by id.", description = "Allows customers to update their booking details.")
     public Object update(@PathVariable Long id, @RequestBody @Valid Object bookingRequestDto) {
         return bookingService.updateById(id, bookingRequestDto);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete booking by id.", description = "Enables the cancellation of bookings.")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable Long id) {
         bookingService.deleteById(id);
     }
 
     @GetMapping
+    @Operation(summary = "Get bookings by customer and status.", description = "Retrieves bookings based on customer ID and their status.")
     public List<Object> getByUserAndStatus(
             @RequestParam Long userId,
             @RequestParam Object status,
