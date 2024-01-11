cf45d5c294380d188a065b1bf1599054682213b1
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-11 21:41:10 +0200

    corrected customer conroller methods

--- src/main/java/com/application/bookingservice/controller/CustomerController.java
+++ src/main/java/com/application/bookingservice/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package com.application.bookingservice.controller;
 
+import com.application.bookingservice.model.Customer;
 import com.application.bookingservice.service.customer.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,18 +33,16 @@ public class CustomerController {
     @GetMapping("/me")
     @Operation(summary = "Get customer.",
             description = "Retrieves the profile information for the currently logged-in customer.")
-    public Object getCustomer() {
-        //Customer customer = (Customer) authentication.getPrincipal();
-        Long customerId = 1L;
-        return customerService.getById(customerId);
+    public Object getCustomer(Authentication authentication) {
+        Customer customer = (Customer) authentication.getPrincipal();
+        return customerService.getById(customer.getId());
     }
 
     @PutMapping("/me")
     @Operation(summary = "Update profile information.",
             description = "Allows customers to update their profile information.")
-    public Object updateProfileInfo(@RequestBody @Valid Object customerRequestDto) {
-        //Customer customer = (Customer) authentication.getPrincipal();
-        Long customerId = 1L;
-        return customerService.updateById(customerId, customerRequestDto);
+    public Object updateProfileInfo(@RequestBody @Valid Object customerRequestDto, Authentication authentication) {
+        Customer customer = (Customer) authentication.getPrincipal();
+        return customerService.updateById(customer.getId(), customerRequestDto);
     }
 }
