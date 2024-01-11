b610935d330d93e043a74cada3b23f97b42639b8
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-01-11 13:32:54 +0200

    realized Accomodation

--- src/main/java/com/application/bookingservice/model/Accommodation.java
+++ src/main/java/com/application/bookingservice/model/Accommodation.java
@@ -1,4 +1,48 @@
 package com.application.bookingservice.model;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Data
+@Table(name = "accommodations")
+@SQLDelete(sql = "UPDATE accommodations SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 public class Accommodation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(nullable = false)
+    private Type type;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id", nullable = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Address address;
+    private String size;
+    private String amenities;
+    private BigDecimal price;
+    private Integer availableUnits;
+    public enum Type {
+        HOUSE,
+        APARTMENT
+    }
 }
