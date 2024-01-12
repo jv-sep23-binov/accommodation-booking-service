581395bd7096e8f0bcaeff61553a58c850a31a98
Igor Korolevich <igorkorolj@gmail.com>
2024-01-12 16:56:22 +0200

    implemented all methods and added updateStatus

--- src/main/java/com/application/bookingservice/controller/BookingController.java
+++ src/main/java/com/application/bookingservice/controller/BookingController.java
@@ -3,6 +3,7 @@ package com.application.bookingservice.controller;
 import com.application.bookingservice.dto.booking.BookingRequestDto;
 import com.application.bookingservice.dto.booking.BookingResponseDto;
 import com.application.bookingservice.dto.booking.BookingUpdateRequestDto;
+import com.application.bookingservice.dto.booking.BookingUpdateStatusRequestDto;
 import com.application.bookingservice.model.Customer;
 import com.application.bookingservice.service.booking.BookingService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,6 +17,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,7 +33,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Booking management.",
         description = "Endpoints for managing bookings.")
 public class BookingController {
-    private BookingService bookingService;
+    private final BookingService bookingService;
+
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    @PatchMapping("/{id}/status")
+    @Operation(summary = "Update booking status.",
+            description = "Allows manager to change booking status.")
+    public BookingResponseDto updateStatus(
+            @PathVariable Long id,
+            @RequestBody @Valid BookingUpdateStatusRequestDto requestDto
+    ) {
+        return bookingService.updateStatus(id, requestDto);
+    }
 
     @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     @PostMapping
