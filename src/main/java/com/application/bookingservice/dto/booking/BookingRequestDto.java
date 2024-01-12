b9080bc2ef8f8bd412be12ec8d76a288483ee852
Igor Korolevich <igorkorolj@gmail.com>
2024-01-12 16:57:17 +0200

    changet @NotBlank to @NotNull

--- src/main/java/com/application/bookingservice/dto/booking/BookingRequestDto.java
+++ src/main/java/com/application/bookingservice/dto/booking/BookingRequestDto.java
@@ -1,20 +1,20 @@
 package com.application.bookingservice.dto.booking;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.time.LocalDate;
 import lombok.Data;
 
 @Data
 public class BookingRequestDto {
-    @NotBlank
+    @NotNull
     @Positive
     private Long accommodationId;
-    @NotBlank
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate checkIn;
-    @NotBlank
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate checkOut;
 }
