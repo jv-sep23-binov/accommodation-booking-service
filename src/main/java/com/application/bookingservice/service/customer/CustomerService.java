d976408b60010cc663b5fc69982c6f98f1903909
Lenovo <liubchenkalexander@gmail.com>
2024-01-15 11:34:48 +0200

    changed CustomerDto in getById and updateById for updateRequest and updateResponse Dtos

--- src/main/java/com/application/bookingservice/service/customer/CustomerService.java
+++ src/main/java/com/application/bookingservice/service/customer/CustomerService.java
@@ -1,9 +1,11 @@
 package com.application.bookingservice.service.customer;
 
-import com.application.bookingservice.dto.customer.CustomerDto;
 import com.application.bookingservice.dto.customer.CustomerRegistrationRequestDto;
 import com.application.bookingservice.dto.customer.CustomerRegistrationResponseDto;
+import com.application.bookingservice.dto.customer.CustomerResponseDto;
 import com.application.bookingservice.dto.customer.CustomerResponseDtoWithRoles;
+import com.application.bookingservice.dto.customer.CustomerUpdateRequestDto;
+import com.application.bookingservice.dto.customer.CustomerUpdateResponseDto;
 import com.application.bookingservice.dto.customer.CustomerUpdateRoleRequestDto;
 
 public interface CustomerService {
@@ -11,7 +13,8 @@ public interface CustomerService {
 
     CustomerResponseDtoWithRoles updateRole(Long id, CustomerUpdateRoleRequestDto updateRequestDto);
 
-    CustomerDto getById(Long customerId);
+    CustomerResponseDto getById(Long customerId);
 
-    CustomerDto updateById(Long customerId, CustomerDto customerDto);
+    CustomerUpdateResponseDto updateById(Long customerId,
+                                         CustomerUpdateRequestDto customerUpdateRequestDto);
 }
