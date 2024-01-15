e49ea4ffee28defce1abf51121196a10c96d597d
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-15 18:52:53 +0200

    added tests for accommodation service

---
+++ src/test/java/com/application/bookingservice/service/address/AddressServiceImplTest.java
@@ -0,0 +1,10 @@
+package com.application.bookingservice.service.address;
+
+import org.junit.jupiter.api.Test;
+
+class AddressServiceImplTest {
+
+    @Test
+    void updateById_ValidId_ReturnAddress() {
+    }
+}
