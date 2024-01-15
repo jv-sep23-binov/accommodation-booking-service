1eff1e7a616c25d7dab6d2fc5588667c5e282e72
Lenovo <liubchenkalexander@gmail.com>
2024-01-15 11:33:28 +0200

    deleted validation in CustomerResponseDtoWithRoles

--- src/main/java/com/application/bookingservice/dto/customer/CustomerResponseDtoWithRoles.java
+++ src/main/java/com/application/bookingservice/dto/customer/CustomerResponseDtoWithRoles.java
@@ -1,22 +1,13 @@
 package com.application.bookingservice.dto.customer;
 
 import com.application.bookingservice.model.Role;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import java.util.Set;
 import lombok.Data;
 
 @Data
 public class CustomerResponseDtoWithRoles {
-    @Email
-    @NotBlank(message = "Name cannot be blank")
     private String email;
-    @Size(max = 50)
-    @NotBlank(message = "Name cannot be blank")
     private String firstName;
-    @Size(max = 50)
-    @NotBlank(message = "Name cannot be blank")
     private String lastName;
     private Set<Role> roles;
 }
