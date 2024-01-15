dfff8e60e735409bfa32547d770452ecf1a16283
Lenovo <liubchenkalexander@gmail.com>
2024-01-15 11:32:52 +0200

    renamed CustomerDto to CustomerResponseDto deleted validation

---
+++ src/main/java/com/application/bookingservice/dto/customer/CustomerResponseDto.java
@@ -0,0 +1,10 @@
+package com.application.bookingservice.dto.customer;
+
+import lombok.Data;
+
+@Data
+public class CustomerResponseDto {
+    private String email;
+    private String firstName;
+    private String lastName;
+}
